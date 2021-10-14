787987968a8be0d83b2ba3621323a5db084a8f2b
Fahid Hossain <fahidhossain2021@gmail.com>
2021-10-14 18:26:04 +0600

    react router hash not working solution with react router HashLink

--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Container, Nav, Navbar } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
+import { HashLink } from 'react-router-hash-link';
 import useAuth from '../../../hooks/useAuth';
 
 const Header = () => {
@@ -13,9 +14,9 @@ const Header = () => {
                     <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                     <Navbar.Collapse className="justify-content-end">
                         <Nav className="ms-auto me-4">
-                            <Nav.Link as={Link} to="/home#home">Home</Nav.Link>
-                            <Nav.Link as={Link} to="/home#services">Services</Nav.Link>
-                            <Nav.Link as={Link} to="/home#experts">Our Experts</Nav.Link>
+                            <Nav.Link as={HashLink} to="/home#home">Home</Nav.Link>
+                            <Nav.Link as={HashLink} to="/home#services">Services</Nav.Link>
+                            <Nav.Link as={HashLink} to="/home#experts">Our Experts</Nav.Link>
                          
                          {
                              !user.email ?     <Nav.Link as={Link} to="/login">Log-In</Nav.Link> :    <button onClick={logOut} className="btn btn-outline-secondary border-2">Sign-Out</button>
